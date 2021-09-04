4280f1a2d730d8d59ff33cde4d77f59fc699a746
evansceo <evans.onyiego@student.moringaschool.com>
2021-09-04 20:53:48 +0300

    function intro

--- cal.js
+++ cal.js
@@ -1,3 +1,5 @@
 var akanMaleName = ["Kwasi!", "Kwadwo!", "Kwabena!", "Kwaku!", "Yaw!", "Kofi!", "Kwame!"]
 var akanFemaleName = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
 
+function checkDayOfMonth() {
+  var century = parseInt(document.getElementById("century").value);
