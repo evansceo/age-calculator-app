4ed89ff04cdb27b8e695e9f4b6de8d35533a0450
evansceo <evans.onyiego@student.moringaschool.com>
2021-09-04 20:58:19 +0300

    cc, yy, mm, dd, d

--- cal.js
+++ cal.js
@@ -7,4 +7,9 @@ function checkDayOfMonth() {
   var month = parseInt(document.getElementById("month").value);
   var dayOfMonth = parseInt(document.getElementById("monthday").value);
   var gender = document.getElementById("gender").value;
-
+  var CC = century;
+  var YY = year;
+  var MM = month;
+  var DD = dayOfMonth;
+  var d = parseInt(((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7
+ 
