5ff33039cb26f2e5d7bba772602a8fcadb21fc87
evansceo <evans.onyiego@student.moringaschool.com>
2021-09-04 20:55:30 +0300

    function-month

--- cal.js
+++ cal.js
@@ -4,4 +4,5 @@ var akanFemaleName = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
 function checkDayOfMonth() {
   var century = parseInt(document.getElementById("century").value);
   var year = parseInt(document.getElementById("year").value);
+  var month = parseInt(document.getElementById("month").value);
   
